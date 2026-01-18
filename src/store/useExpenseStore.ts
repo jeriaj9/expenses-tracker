1cea4827edea0d986ada89790d638efa0cc838bd
Webuddy <webuddyrd@gmail.com>
2026-01-18 10:00:00 -0400

    feat: add async persistence to expense store

--- src/store/useExpenseStore.ts
+++ src/store/useExpenseStore.ts
@@ -1,4 +1,6 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Expense } from '../types/expense';
 
 interface ExpenseState {
@@ -9,19 +11,27 @@ interface ExpenseState {
     getExpenseById: (id: string) => Expense | undefined;
 }
 
-export const useExpenseStore = create<ExpenseState>((set, get) => ({
-    expenses: [],
-    addExpense: (expense) =>
-        set((state) => ({ expenses: [...state.expenses, expense] })),
-    removeExpense: (id) =>
-        set((state) => ({
-            expenses: state.expenses.filter((e) => e.id !== id),
-        })),
-    updateExpense: (id, updatedExpense) =>
-        set((state) => ({
-            expenses: state.expenses.map((e) =>
-                e.id === id ? { ...e, ...updatedExpense } : e
-            ),
-        })),
-    getExpenseById: (id) => get().expenses.find((e) => e.id === id),
-}));
+export const useExpenseStore = create<ExpenseState>()(
+    persist(
+        (set, get) => ({
+            expenses: [],
+            addExpense: (expense) =>
+                set((state) => ({ expenses: [...state.expenses, expense] })),
+            removeExpense: (id) =>
+                set((state) => ({
+                    expenses: state.expenses.filter((e) => e.id !== id),
+                })),
+            updateExpense: (id, updatedExpense) =>
+                set((state) => ({
+                    expenses: state.expenses.map((e) =>
+                        e.id === id ? { ...e, ...updatedExpense } : e
+                    ),
+                })),
+            getExpenseById: (id) => get().expenses.find((e) => e.id === id),
+        }),
+        {
+            name: 'expense-storage',
+            storage: createJSONStorage(() => AsyncStorage),
+        }
+    )
+);
